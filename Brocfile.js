0495966c472ea8057ac3e3b1adb55d8374a110cd
Steven Lindberg <stevenjohnlindberg@gmail.com>
2014-04-17 09:39:35 -0700

    Added broccoli file

---
+++ Brocfile.js
@@ -0,0 +1,49 @@
+var fs = require('fs');
+var mergeTrees = require('broccoli-merge-trees');
+var pickFiles = require('broccoli-static-compiler');
+// var compileTemplates = require('broccoli-template');
+var compileModules = require('broccoli-dist-es6-module');
+
+// Source files are located in the 'package' directory
+var srcTree = 'packages';
+
+// Find all package names
+var packages = fs.readdirSync(srcTree).filter(function(name) {
+  return fs.statSync(srcTree + '/' + name).isDirectory();
+});
+
+// Create a tree for each package that only uses files in the 'lib' subdirectory
+packages = packages.map(function(name) {
+  return pickFiles(srcTree + '/' + name, {
+    srcDir: '/lib',
+    destDir: '/' + name,
+  });
+});
+
+// Pick out the 'main.js' entry point file
+srcTree = pickFiles(srcTree, {
+  srcDir: '/',
+  files: [ 'main.js' ],
+  destDir: '/',
+});
+
+// Merge them all together
+srcTree = mergeTrees(packages.concat(srcTree));
+
+// Compile templates
+// srcTree = compileTemplates(srcTree, {
+//   extensions: [ 'hbs' ],
+//   compileFunction: 'Ember.Handlebars.compile',
+// });
+
+// Compile modules into different formats
+var moduleTree = compileModules(srcTree, {
+  global: 'Lytics.Components',
+  packageName: 'lytics-components',
+  main: 'main',
+  shim: {
+    'ember': 'Ember'
+  }
+});
+
+module.exports = mergeTrees(moduleTree);
--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Lytics Ember Components
 
 This is a collection of generic input components used by Lytics Inc.
+
+## Building
+
+Install [Broccoli](https://github.com/joliss/broccoli) globally if needed:
+
+```sh
+$ npm install -g broccoli-cli
+```
+
+Build to the `dist/` directory:
+
+```sh
+$ npm install
+$ broccoli build dist
+```
