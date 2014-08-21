27d1bfffed076daff081060dfa392ed670ee13f4
Michael Lange <dingoeatingfuzz@gmail.com>
2014-08-21 16:32:27 -0700

    Allow for aligning popovers to their parent
    By default, popovers align to the window. Sometimes it is
    desired to have the popover align to its parent.

--- packages/display/lib/popover.js
+++ packages/display/lib/popover.js
@@ -45,6 +45,8 @@ export default Component.extend(ParentComponentMixin, ChildComponentMixin, Activ
 
   anchor: null,
 
+  alignToParent: false,
+
   //
   // Internal Properties
   //
@@ -153,7 +155,8 @@ export default Component.extend(ParentComponentMixin, ChildComponentMixin, Activ
       var $el = this.$();
       var $arrow = $el.find('.arrow');
       var $anchor = $(get(this, 'anchor'));
-      var anchorOffset = $anchor.offset() || { top: 0, left: 0 };
+      var anchorOffset = get(this, 'alignToParent') ? $anchor.position() : $anchor.offset();
+      anchorOffset = anchorOffset || { top: 0, left: 0 };
 
       setProperties(this, {
         offsetTop: anchorOffset.top,
